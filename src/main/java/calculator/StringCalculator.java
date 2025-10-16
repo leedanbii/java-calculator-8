08bd92b05b1076adefa6d87ed70b9a3ddad37bcd
leedanbii <eksqlgodqhr@naver.com>
2025-10-16 19:48:45 +0900

    feat: 기본 구분자(, :)로 문자열 분리 및 덧셈 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,7 +5,23 @@ public class StringCalculator {
     	if(input == null || input.isEmpty()) {
     		return 0;
     	}
-    	// 이후에 확장
-    	return Integer.parseInt(input);
+    	String[] numbers = splitNumbers(input);
+    	return sum(numbers);
     }
+	
+	private static String[] splitNumbers(String input) {
+		//커스텀 구분자 추후 확장
+		return input.split(",|:");
+	}
+	
+	private static int sum(String[] numbers) {
+		int total = 0;
+		for(String numStr: numbers) {
+			if(numStr.isEmpty()) continue;
+			int number = Integer.parseInt(numStr);
+			//예외처리 추후 확장
+			total += number;
+		}
+		return total;
+	}
 }
